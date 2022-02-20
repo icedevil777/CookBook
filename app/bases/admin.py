7759419d642c9b1ce638ee7bf71b44f847e10029
icedevil777111 <icedevil777111@mail.ru>
2022-02-20 19:08:52 +0300

    JS_Filter v3.0

--- app/bases/admin.py
+++ app/bases/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-
+from .models import Ingredient
 from .models import Recipes
+
 
 @admin.register(Recipes)
 class RecipesAdmin(admin.ModelAdmin):
     list_display = ['name', 'description']
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- app/bases/models.py
+++ app/bases/models.py
@@ -6,9 +6,19 @@ class Recipes(models.Model):
     ingredients = models.ManyToManyField('Ingredient', blank=False)
     description = models.TextField()
 
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
